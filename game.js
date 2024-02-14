fc25033b42741af219c93fc21283fb8b6fddbe61
BB-Github-Account <55419782+BB-Github-Account@users.noreply.github.com>
2024-02-13 23:52:25 -0600

    Add playGame function and update playRound Plays 5 rounds of game and displays outcome of each round

--- game.js
+++ game.js
@@ -1,4 +1,5 @@
 const rps = ["rock", "paper", "scissors"];
+let gamesWon = 0;
 
 function getComputerChoice(){
     return  rps[Math.floor(Math.random() * rps.length)];
@@ -9,29 +10,46 @@ function playRound(playerSelection, computerSelection){
     const lowerPlayerSel = playerSelection.toLowerCase();
     const lowerComputerSel = computerSelection.toLowerCase();
 
-    if(lowerPlayerSel == lowerComputerSel) {
-        console.log(`It's a tie, both selected ${lowerPlayerSel}`);
-        return `It's a tie, both selected ${lowerPlayerSel}`;
+    if(lowerPlayerSel != "rock" && lowerPlayerSel != "paper" && lowerPlayerSel != "scissors"){return alert("Invalid input, the only options are rock, paper, or scissors");}
+
+    if(lowerPlayerSel == lowerComputerSel) {        
+        return alert(`It's a tie, both played ${lowerPlayerSel}`);
     }
     else if(lowerPlayerSel == "rock") {
-        if(lowerComputerSel == "paper"){
-            return `You lose, ${lowerComputerSel} beats ${lowerPlayerSel}`;
+        if(lowerComputerSel == "paper") {
+            return alert(`You lose, ${lowerComputerSel} beats ${lowerPlayerSel}`);
         } 
-        else { return `You win, ${lowerPlayerSel} beats ${lowerComputerSel}`;}
-                
+        else { 
+            gamesWon++;
+             return alert(`You win, ${lowerPlayerSel} beats ${lowerComputerSel}`);
+        }  
     }
     else if(lowerPlayerSel == "paper") {
-        if(lowerComputerSel == "scissors"){
-            return `You lose, ${lowerComputerSel} beats ${lowerPlayerSel}`;
+        if(lowerComputerSel == "scissors") {
+            return alert(`You lose, ${lowerComputerSel} beats ${lowerPlayerSel}`);        
         } 
-        else { return `You win, ${lowerPlayerSel} beats ${lowerComputerSel}`;}
-        
+        else { 
+            gamesWon++; 
+            return alert(`You win, ${lowerPlayerSel} beats ${lowerComputerSel}`);
+        }        
     }
     else if(lowerPlayerSel == "scissors") {
-        if(lowerComputerSel == "rock"){
-            return `You lose, ${lowerComputerSel} beats ${lowerPlayerSel}`;
+        if(lowerComputerSel == "rock") {
+            return alert(`You lose, ${lowerComputerSel} beats ${lowerPlayerSel}`);
         } 
-        else { return `You win, ${lowerPlayerSel} beats ${lowerComputerSel}`;}
-                
+        else { 
+            gamesWon++; 
+            return alert(`You win, ${lowerPlayerSel} beats ${lowerComputerSel}`);
+        }                
     }
 }
+
+function playGame(){
+    gamesWon = 0;
+    for(let i = 0; i < 5; i++){
+        playRound(prompt("Rock, Paper, or Scissors"), getComputerChoice());
+    }
+    alert(`You won ${gamesWon} out of 5 rounds`);
+}
+
+playGame();
