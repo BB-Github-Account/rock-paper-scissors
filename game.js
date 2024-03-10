f989e0be76e1242f1a6b0871299d6472af8bfd0b
BB-Github-Account <55419782+BB-Github-Account@users.noreply.github.com>
2024-03-09 21:13:52 -0600

    Update playRound function Removed redundant code by adding showScore function

--- game.js
+++ game.js
@@ -30,74 +30,57 @@ function playRound(playerSelection, computerSelection){
 
     if(totalGames < 5){
         if(lowerPlayerSel == lowerComputerSel) { 
-            totalGames++;
-            score.textContent = `Won ${gamesWon} out of ${totalGames}`;       
+            showScore(false);      
             return `It's a tie, both played ${lowerPlayerSel}`;
         }
         else if(lowerPlayerSel == "rock") {
             if(lowerComputerSel == "paper") {
-                totalGames++;
-                score.textContent = `Won ${gamesWon} out of ${totalGames}`;
+                showScore(false);
                 return `You lose, ${lowerComputerSel} beats ${lowerPlayerSel}`;
             } 
             else { 
-                gamesWon++;
-                totalGames++;
-                score.textContent = `Won ${gamesWon} out of ${totalGames}`;
+                showScore(true);
                 return `You win, ${lowerPlayerSel} beats ${lowerComputerSel}`;
             }  
         }
         else if(lowerPlayerSel == "paper") {
             if(lowerComputerSel == "scissors") {
-                totalGames++;
-                score.textContent = `Won ${gamesWon} out of ${totalGames}`;
+                showScore(false);
                 return `You lose, ${lowerComputerSel} beats ${lowerPlayerSel}`;        
             } 
             else { 
-                gamesWon++; 
-                totalGames++;
-                score.textContent = `Won ${gamesWon} out of ${totalGames}`;
+                showScore(true);
                 return `You win, ${lowerPlayerSel} beats ${lowerComputerSel}`;
             }        
         }
         else if(lowerPlayerSel == "scissors") {
             if(lowerComputerSel == "rock") {
-                totalGames++;
-                score.textContent = `Won ${gamesWon} out of ${totalGames}`;
+                showScore(false);
                 return `You lose, ${lowerComputerSel} beats ${lowerPlayerSel}`;
             } 
             else { 
-                gamesWon++; 
-                totalGames++;
-                score.textContent = `Won ${gamesWon} out of ${totalGames}`;
+                showScore(true);
                 return `You win, ${lowerPlayerSel} beats ${lowerComputerSel}`;
             }                
         }
     } 
-    else {
-        if(gamesWon >= 3) {
-            gamesWon = 0;
-            totalGames = 0;
-            return `Congratulations, You win`; 
+
+    function showScore(won){
+        if(won == true){
+            gamesWon++;        
         }
-        else {
+        totalGames++;
+        score.textContent = `Won ${gamesWon} out of ${totalGames}`;
+
+        if(totalGames == 5) {            
+            if(gamesWon >= 3) {              
+                score.textContent = `Congratulations, You win`; 
+            }
+            else {                
+                score.textContent = `Sorry you lose`; 
+            }
             gamesWon = 0;
             totalGames = 0;
-            return `Sorry you lose`; 
         }
     }
 }
-
-function playGame(){
-    gamesWon = 0;
-    //for(let i = 0; i < 5; i++){
-    //    playRound(prompt("Rock, Paper, or Scissors"), getComputerChoice());
-    //}
-
-    playRound(prompt("Rock, Paper, or Scissors"), getComputerChoice());
-    alert(`You won ${gamesWon} out of 5 rounds`);
-
-
-}
-
-//playGame();
